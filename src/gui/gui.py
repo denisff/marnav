2f2bc84c2f776f81553ed99938518a6ab6c9e967
juha.niskanen <juha.niskanen@2649799f-fd4f-0410-bf84-09c4532b2003>
2009-01-06 08:21:45 +0000

    Changed to executable

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+import pygtk
+import gtk
 import gtk.glade
 
 
@@ -36,6 +39,7 @@ class UI(object):
         self.tree = gtk.glade.XML("avNav.glade")
         self.nb = self.tree.get_widget("notebook1")
         self.w1 = self.tree.get_widget("windowMain")
+        self.tree.signal_autoconnect(self)
         self.w1.connect("delete_event", lambda w,e: gtk.main_quit())
         self.w1.connect('key_press_event', on_key_press)
         self.w1.show()
